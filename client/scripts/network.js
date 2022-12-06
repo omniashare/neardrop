9fc81fbc288938b189d5acbd7789a96f09277e2d
MWY001 <mwy001@outlook.com>
2022-12-06 20:24:21 +0800

    Add turn servers

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -540,7 +540,24 @@ class Events {
 
 RTCPeer.config = {
     'sdpSemantics': 'unified-plan',
-    'iceServers': [{
-        urls: 'stun:stun.l.google.com:19302'
-    }]
+    'iceServers': [
+        {
+            urls: 'stun:stun.l.google.com:19302',
+        },
+        {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
+        {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+        },
+    ],
 }
