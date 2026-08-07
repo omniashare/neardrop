0954d4a1366d6c76585dfc1a3583e57bd63b870f
wangqiao <qiao.wang@dewmobile.net>
2026-08-08 00:23:44 +0800

    wq

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -32,8 +32,8 @@ class ServerConnection {
         const lastDisplayName = localStorage.getItem('displayname')
         const roomid = localStorage.getItem('roomnumber')?localStorage.getItem('roomnumber'):''
         Events.fire('room-display',roomid)
-       // const ws = lastDisplayName ? new WebSocket(this._endpoint()+'?lastDisplayName='+lastDisplayName+'&room='+roomid) : new WebSocket(this._endpoint()+'?room='+roomid)
-       const ws = lastDisplayName ?new WebSocket('ws://localhost:3000/server/webrtc?lastDisplayName='+encodeURIComponent(lastDisplayName)+'&room='+encodeURIComponent(roomid)):new WebSocket('ws://localhost:3000/server/webrtc?room='+encodeURIComponent(roomid))
+        const ws = lastDisplayName ? new WebSocket(this._endpoint()+'?lastDisplayName='+lastDisplayName+'&room='+roomid) : new WebSocket(this._endpoint()+'?room='+roomid)
+       //const ws = lastDisplayName ?new WebSocket('ws://localhost:3000/server/webrtc?lastDisplayName='+encodeURIComponent(lastDisplayName)+'&room='+encodeURIComponent(roomid)):new WebSocket('ws://localhost:3000/server/webrtc?room='+encodeURIComponent(roomid))
         ws.binaryType = 'arraybuffer';
         ws.onopen = e => {
             console.log('WS: server connected');
